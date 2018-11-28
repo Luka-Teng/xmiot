c1309a2bab59935ace4902c9d193c3c1a6c48aa0
Luka <luka.teng@ximalaya.com>
2018-11-28 21:10:16 +0800

    test dep

--- packages/xmiot-template/src/index.js
+++ packages/xmiot-template/src/index.js
@@ -1,9 +1,11 @@
 import './index.less'
 // import pic from './bg.png'
+import fn from 'xmiot-function'
 import React from 'react'
 window.env = process.env.NODE_ENV
 class Test extends React.Component {
   render () {
+    console.log(fn)
     return <div>11111111211</div>
   }
 }
