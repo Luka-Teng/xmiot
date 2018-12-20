2120aa9e9b80b17f8f056abdeab4a8fdf3254018
Luka <luka.teng@ximalaya.com>
2018-12-20 11:22:32 +0800

    xmiot-net: request拦截器的错误和网络请求错误的区分

--- packages/xmiot-net/src/index.js
+++ packages/xmiot-net/src/index.js
@@ -87,6 +87,17 @@ class Net {
         return result || response
       },
       async err => {
+        /**
+         * 判断是否是err是否携带config来判断是否是请求前的程序错误，还是请求后网络错误。
+         * 如果是请求前的程序错误则会打乱调用链，抛出错误，提醒开发者
+         */
+        if (!err.config) {
+          console.log(
+            '这是请求前的程序错误，会破坏后置调用链，建议先修复bug，再刷新页面'
+          )
+          return Promise.reject(err)
+        }
+
         // err.notError 来判断是否是错误的
         err.notAErrorInError = false
         const result = await promiseSequence(
