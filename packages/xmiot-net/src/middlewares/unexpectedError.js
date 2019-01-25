2965be357f17c8fefb9faea56f2afc4d31e9cbe1
Luka <luka.teng@ximalaya.com>
2019-01-25 18:49:51 +0800

    temp

--- packages/xmiot-net/src/middlewares/unexpectedError.js
+++ packages/xmiot-net/src/middlewares/unexpectedError.js
@@ -6,7 +6,7 @@ export default net => {
      * 如果是请求前的程序错误则会打乱调用链，抛出错误，提醒开发者
      */
     if (!error.config && !error.response) {
-      console.log(
+      console.error(
         '这是请求前的程序错误，会破坏后置调用链，建议先修复bug，再刷新页面'
       )
       throw error
