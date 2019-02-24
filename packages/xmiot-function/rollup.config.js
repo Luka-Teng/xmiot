ff08665b2eef204113ac71baec12f115fd7ea7e6
Luka <luka.teng@ximalaya.com>
2019-02-24 17:55:31 +0800

    xmiot-function rollup.config

--- packages/xmiot-function/rollup.config.js
+++ packages/xmiot-function/rollup.config.js
@@ -1,10 +1,18 @@
 const getRollupConfig = require('../../utils/getRollupConfig')
 
+const fileName = module => {
+  const strategy = {
+    cjs: 'index.common.js',
+    es: 'index.esm.js'
+  }
+  return strategy[module]
+}
+
 function getOptions (module) {
   return {
     input: './src/index.ts',
     output: {
-      file: './entry/index.common.js',
+      file: `./entry/${fileName(module)}`,
       format: module
     }
   }
