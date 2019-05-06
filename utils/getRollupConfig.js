b409e0e5e51f0b0cb8cd41a0ef3296abc1118dd1
Luka <luka.teng@ximalaya.com>
2019-05-06 14:25:18 +0800

    update: 优化打包流程

--- utils/getRollupConfig.js
+++ utils/getRollupConfig.js
@@ -4,69 +4,44 @@
  * 默认API_ENV配置为development
  * params: type: global, react
  */
+process.env.NODE_ENV = 'production'
+
 const babel = require('rollup-plugin-babel')
 const path = require('path')
-// const commonjs = require('rollup-plugin-commonjs')
 const postcss = require('rollup-plugin-postcss')
 const url = require('postcss-url')
 const fileAsBlob = require('rollup-plugin-file-as-blob')
 const replace = require('rollup-plugin-replace')
-const { eslint } = require('rollup-plugin-eslint')
 const { getClientEnvironment } = require('./env')
 const { multiDeepAssign } = require('./function')
-const typescript = require('rollup-plugin-typescript2')
 const json = require('rollup-plugin-json')
-
-// const namedExports = require('./namedExports')
-
-/*
- * eslint的配置在每个包中，由包主人自行管理
- * eslint的插件包，preset包由lerna统一管理
- */
-function esLintConfig (packageDir) {
-  return eslint({
-    // 只允许eslint当前包
-    include: path.resolve(packageDir, 'src/**/*.js'),
-    cwd: packageDir
-  })
-}
 
 module.exports = (
   options = {},
   { type = 'global', packageDir = '', isTypeScript = false, noCss = false } = {}
 ) => {
-  const plugins = [
-    // 环境变量的定义
-    replace({
-      ...getClientEnvironment().stringified
-      // 不包括除了react之外的包
-      // exclude: /node_modules\/(?!react\/).*/
-    })
-  ]
-
-  if (!isTypeScript) {
-    // 不是 TypeScript 就是 JavaScript
-    plugins.push(esLintConfig(packageDir))
-  } else {
-    // Typescript 配置
-    plugins.push(
-      typescript({
-        clean: true,
-        abortOnError: false
-      })
-    )
-  }
 
   const defaultOptions = {
     plugins: [
-      ...plugins,
+      {
+        resolveId: (i, e) => {
+          console.log(i)
+          console.log(e)
+        }
+      },
+      replace({
+        ...getClientEnvironment().stringified
+        // 不包括除了react之外的包
+        // exclude: /node_modules\/(?!react\/).*/
+      }),
 
       // babel先对react进行转义,只有在react环境中执行
       babel({
         babelrc: false,
-        presets: ['@babel/preset-react'],
+        presets: [['react-app', { flow: false, typescript: true }]],
         exclude: 'node_modules/**',
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        runtimeHelpers: true,
         plugins: [
           [path.resolve(__dirname, 'babel-plugin-require-to-import')],
           ['@babel/plugin-proposal-class-properties'],
@@ -80,12 +55,6 @@ module.exports = (
           ]
         ]
       }),
-
-      // Convert CommonJS modules to ES6
-      // 增加对react的Component的导出
-      // commonjs(
-      //   type === 'react' ? { namedExports: namedExports(packageDir) } : {}
-      // ),
 
       // read json as es6 module
       json(),
