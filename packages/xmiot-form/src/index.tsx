3d77499f7f90918a62c9d272e7f3fdda5e99a0ff
Luka <luka.teng@ximalaya.com>
2019-05-10 14:37:31 +0800

    update: 处理错误边界

--- packages/xmiot-form/src/index.tsx
+++ packages/xmiot-form/src/index.tsx
@@ -3,4 +3,28 @@ import React, { Component } from 'react'
 import './assets/index.less'
 import Form from './form'
 
-export default Form
+import { FormProps } from './types'
+
+export default class Index extends Component<
+  Pick<
+    FormProps,
+    | 'options'
+    | 'onSubmit'
+    | 'confirmButton'
+    | 'extraButtons'
+    | 'formRef'
+    | 'styles'
+  >
+> {
+  state = { hasError: false }
+
+  static getDerivedStateFromError () {
+    return { hasError: true }
+  }
+  componentDidCatch (error: any, info: any) {
+    console.error(error)
+  }
+  render () {
+    return this.state.hasError ? <div /> : <Form {...this.props} />
+  }
+}
