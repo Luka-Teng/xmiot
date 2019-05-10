6bfbd8c8fdb539f42d262ee65007177ac963dca1
Luka <luka.teng@ximalaya.com>
2019-05-10 10:34:28 +0800

    update: xmiot-form custome

--- packages/xmiot-form/src/components/custom.tsx
+++ packages/xmiot-form/src/components/custom.tsx
@@ -2,23 +2,12 @@ import React, { Component } from 'react'
 import { wrapField } from './utils'
 import { FormItemProps } from '../types'
 
-
 class Button extends Component<FormItemProps<'custom'>> {
-  render() {
+  render () {
     const { name, config, styles, label } = this.props
     if (!config) throw new Error('custom组件必须要有config')
     const { render } = config
-    return wrapField(
-      render(),
-      {
-        itemStyle: {
-          textAlign: 'right'
-        },
-        ...styles
-      },
-      Math.random().toLocaleString(),
-      label
-    )
+    return wrapField(render(), styles, Math.random().toLocaleString(), label)
   }
 }
 
