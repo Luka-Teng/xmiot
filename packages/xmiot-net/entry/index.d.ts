3f50715b30b48ecd5833efe56659a6220606df37
luka.teng <luka.teng@ximalaya.com>
2019-06-19 23:46:03 +0800

    bugfix: xmiot-net mock

--- packages/xmiot-net/entry/index.d.ts
+++ packages/xmiot-net/entry/index.d.ts
@@ -3,20 +3,27 @@ declare type genObject = {
 }
 
 interface VerbMock {
-  (url: string, body: {
-    data?: genObject
-    params?: genObject
-  }): {
+  (
+    url: string,
+    body?: {
+      data?: genObject
+      params?: genObject
+    }
+  ): {
     reply: Reply
     replyOnce: Reply
   }
 }
 
 interface Reply {
-  (code: number, response: any, options?: {
-    delay?: number
-    errorMessage?: string
-  }): Net
+  (
+    code: number,
+    response: any,
+    options?: {
+      delay?: number
+      errorMessage?: string
+    }
+  ): Net
 }
 
 declare class Net {
@@ -27,31 +34,39 @@ declare class Net {
   public pre: (callback: (config: genObject, stop: Function) => any) => Net
 
   /* net.postSuccess((response, stop) => {}) */
-  public postSuccess: (callback: (response: genObject, stop: Function) => any) => Net
+  public postSuccess: (
+    callback: (response: genObject, stop: Function) => any
+  ) => Net
 
   /* net.postError((error, stop) => {}) */
   public postError: (callback: (error: genObject, stop: Function) => any) => Net
 
   /* net.onCache */
-  public onCache: (options: {
-    url: string
-    method: string
-    timeout: number
-    ignoreParams: string[]
-    ignoreData: string[]
-  }) => void
+  public onCache: (
+    options: {
+      url: string
+      method: string
+      timeout: number
+      ignoreParams: string[]
+      ignoreData: string[]
+    }
+  ) => void
 
   /* net.prevent */
-  public prevent: (options: {
-    ignores: string[]
-  }) => void
+  public prevent: (
+    options: {
+      ignores: string[]
+    }
+  ) => void
 
   /* net.onResend */
-  public onResend: (options: {
-    url: string,
-    method: string,
-    times: number
-  }) => void
+  public onResend: (
+    options: {
+      url: string
+      method: string
+      times: number
+    }
+  ) => void
 
   /* net.mockBase */
   public mockBase: (baseUrls: string[]) => void
