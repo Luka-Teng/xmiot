3d27ec51eb8eeb887ae6b417b6c688a10f10513b
luka.teng <luka.teng@ximalaya.com>
2019-09-25 15:49:36 +0800

    feat(add): 增加模块xy-ui

---
+++ packages/xy-ui/entry/es/Animation.js
@@ -0,0 +1,5 @@
+function __$strToBlobUri(str, mime, isBinary) {try {return window.URL.createObjectURL(new Blob([Uint8Array.from(str.split('').map(function(c) {return c.charCodeAt(0)}))], {type: mime}));} catch (e) {return "data:" + mime + (isBinary ? ";base64," : ",") + str;}}
+
+var index = 111;
+
+export default index;
---
+++ packages/xy-ui/rollup.config.js
@@ -0,0 +1,55 @@
+const getRollupConfig = require('../../utils/getRollupConfig')
+const path = require('path')
+const fs = require('fs')
+
+const src = path.resolve(__dirname, './src')
+
+/* 读取src文件下的目录 */
+const srcContents = fs.readdirSync(src)
+
+const validDirs = srcContents.map((content, index) => {
+  const dir = path.join(src, content)
+
+  /* 判断是否是文件夹 */
+  if (fs.statSync(dir).isFile()) {
+    return null
+  }
+
+  /* 判断文件夹内部是否有入口文件 */
+  try {
+    if (fs.statSync(path.join(dir, 'index.ts')).isFile()) {
+      return [content, 'index.ts']
+    }
+  } catch {}
+
+  try {
+    if (fs.statSync(path.join(dir, 'index.tsx')).isFile()) {
+      return [content, 'index.tsx']
+    }
+  } catch {}
+
+  return null
+}).filter(Boolean)
+
+const mapToInput = (dir, modules) => {
+  return modules.reduce((result, module) => {
+    result[`${dir}/${module[0]}`] = `${src}/${module[0]}/${module[1]}`
+    return result
+  }, {})
+}
+function getOptions (format) {
+  return {
+    input: mapToInput(format, validDirs),
+    output: {
+      entryFileNames: `[name].js`,
+      chunkFileNames: 'common/[name].js',
+      dir: './entry',
+      format
+    }
+  }
+}
+
+export default [
+  getRollupConfig(getOptions('cjs')),
+  getRollupConfig(getOptions('es'))
+]
