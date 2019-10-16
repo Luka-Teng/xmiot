ba940ae0707b0f445aa695b970b9c3cdfcf4ea88
luka.teng <luka.teng@ximalaya.com>
2019-10-16 11:18:36 +0800

    chore: 增加commit规范

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,10 @@ module.exports = {
   rules: {
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['chore', 'module', 'bugfix', 'refactor']]
+    'type-enum': [
+      2,
+      'always',
+      ['chore', 'module', 'bugfix', 'refactor', 'Publish']
+    ]
   }
 }
